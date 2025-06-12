56f15febb8e8893fa9736c609093311a78c1d05b
Mahaprasad711 <Mahaprasadjata99@gmail.com>
2025-06-12 02:02:06 +0200

    dockerize client and server, deploy working Kubernetes setup with client-server inference

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Waiting for server to start on port 8001..."
 
 # Loop until we can connect to the server
-while ! nc -z server 8001; do
+while ! nc -z resnet-server-service 8001; do
   sleep 1
 done
 
